7bff653419f326628bc08891c7f104d8bcbede23
eduardomsj <eduardomsj@gmail.com>
2020-10-03 01:00:52 -0300

    Delete category

--- backend/src/main/java/com/eduardojr/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/eduardojr/dscatalog/services/CategoryService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.eduardojr.dscatalog.dto.CategoryDTO;
 import com.eduardojr.dscatalog.entities.Category;
 import com.eduardojr.dscatalog.repositories.CategoryRepository;
+import com.eduardojr.dscatalog.services.exceptions.DatabaseException;
 import com.eduardojr.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -54,5 +57,17 @@ public class CategoryService {
 		catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found" + id);
 		}		
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found" + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
 	}
 }
