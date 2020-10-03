e8e52e5eec5abecdd983a36c638c2804d9b2f19d
eduardomsj <eduardomsj@gmail.com>
2020-10-03 18:44:51 -0300

    Product repository, service, resource, GET ok

---
+++ backend/src/main/java/com/eduardojr/dscatalog/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.eduardojr.dscatalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.eduardojr.dscatalog.entities.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
