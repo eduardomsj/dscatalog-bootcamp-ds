365ab89c16393a53fd1d1425b3501d11d912827d
eduardomsj <eduardomsj@gmail.com>
2020-10-24 18:14:03 -0300

    UserUpdateValidator

---
+++ backend/src/main/java/com/eduardojr/dscatalog/dto/UserUpdateDTO.java
@@ -0,0 +1,9 @@
+package com.eduardojr.dscatalog.dto;
+
+import com.eduardojr.dscatalog.services.validation.UserUpdateValid;
+
+@UserUpdateValid
+public class UserUpdateDTO extends UserDTO {
+	private static final long serialVersionUID = 1L;
+		
+}
--- backend/src/main/java/com/eduardojr/dscatalog/services/UserService.java
+++ backend/src/main/java/com/eduardojr/dscatalog/services/UserService.java
@@ -16,6 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.eduardojr.dscatalog.dto.RoleDTO;
 import com.eduardojr.dscatalog.dto.UserDTO;
 import com.eduardojr.dscatalog.dto.UserInsertDTO;
+import com.eduardojr.dscatalog.dto.UserUpdateDTO;
 import com.eduardojr.dscatalog.entities.Role;
 import com.eduardojr.dscatalog.entities.User;
 import com.eduardojr.dscatalog.repositories.RoleRepository;
@@ -59,7 +60,7 @@ public class UserService {
 	}
 
 	@Transactional
-	public UserDTO update(Long id, UserDTO dto) {
+	public UserDTO update(Long id, UserUpdateDTO dto) {
 		try {
 			User entity = repository.getOne(id);
 			copyDtoToEntity(dto, entity);
