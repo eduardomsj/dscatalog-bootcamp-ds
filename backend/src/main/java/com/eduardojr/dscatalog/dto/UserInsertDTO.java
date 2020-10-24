4a7d5817abc73a7d099b8a801fab70a965ee9561
eduardomsj <eduardomsj@gmail.com>
2020-10-24 17:18:20 -0300

    Custom ConstraintValidator

--- backend/src/main/java/com/eduardojr/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/eduardojr/dscatalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.eduardojr.dscatalog.dto;
 
+import com.eduardojr.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO {
 	private static final long serialVersionUID = 1L;
 
